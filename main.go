e9850f7e6d0eed3bb733467d88dea0b762ae2688
tuantv-theinfitech <tuan_tran@theinfitech.com>
2025-08-07 00:59:26 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"image/color"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
+)
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	// TODO: Handle input, update game state, etc.
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	// Clear background
+	screen.Fill(color.RGBA{30, 30, 30, 255}) // Dark gray background
+
+	// Draw border rectangle (play area)
+	borderColor := color.White
+	x, y := float32(50.0), float32(50.0)
+	w, h := float32(540.0), float32(380.0)
+	vector.DrawFilledRect(screen, x, y, w, 1, borderColor, false)       // Top
+	vector.DrawFilledRect(screen, x, y+h-1, w, 1, borderColor, false)   // Bottom
+	vector.DrawFilledRect(screen, x, y, 1, h, borderColor, false)       // Left
+	vector.DrawFilledRect(screen, x+w-1, y, 1, h, borderColor, false)   // Right
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	// Return the size of the game area
+	return 640, 480 // Fixed size for simplicity
+}
+
+func main() {
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Devide Game")
+	game := &Game{}
+
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
+}
